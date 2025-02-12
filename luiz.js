0544dd1efe1f2df9d6e19016725795c9fb4b079a
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-02-12 12:16:16 -0300

    Add files via upload

---
+++ luiz.js
@@ -0,0 +1,21 @@
+let n = "Luiz";
+let s = " Guilherme Coelho";
+let i = 16;
+let a = 1.75;
+let h = ["futebol e videogame"];
+let e = true;
+let t = true;
+let soma = n + s;
+
+console.log("Olá, meu nome é "+ soma + " tenho " + i + " anos e " + a + " metros de altura");
+console.log("Meus hobbies são " + h );
+if(e == true){
+    console.log("Eu estudo!")
+
+} else {
+    console.log("E não estuda !")
+}
+
+if(t == true){
+    console.log("Eu trabalho!")
+}
