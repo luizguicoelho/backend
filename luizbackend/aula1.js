c088826651e3b7473990d8263324347afffd3135
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-02-21 11:52:43 -0300

    Add files via upload

---
+++ luizbackend/aula1.js
@@ -0,0 +1,11 @@
+const v1 = 12;
+const v2 = 0;
+let s = v1 + v2;
+let sub = v1 - v2;
+let m = v1 * v2;
+let d = v1 / v2;
+
+console.log("A soma é," + s);
+console.log("A subtração é," + sub);
+console.log("A muitiplicação é," + m);
+console.log("A divisão é," + d );
---
+++ luizbackend/aula2.js
@@ -0,0 +1,14 @@
+let est = true;
+let ests = false;
+
+//operador de igualdade:
+//compara apenas o valor
+console.log(true == 1);
+console.log(false == 0);
+console.log("1 " == 1);
+
+//operedor de igualdeda estrta:
+//compara valor e tipo de dados
+console.log(true === 1);
+console.log(false === 0);
+console.log("1" === 1);
