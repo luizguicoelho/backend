1bfaf0b67c37a2217dd12d3ace9f89942229a5da
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-03-07 12:13:03 -0300

    Add files via upload

---
+++ luizbackend/aula5.js
@@ -0,0 +1,8 @@
+let esp = ["coxinha de frango com catupiri", "pastel", "coxinha 500g", "coxinha 1kg", "risoles",]
+let vlr = ["2,5", "5", "14,5", "32,95", "3"]
+ct = 0;
+while(ct < esp.length){
+    let ct2 = ct + 1
+    console.log(ct2 + " - " + esp[ct] + " R$ " + vlr[ct])
+    ct = ct + 1
+}
--- luizbackend/exe1.js
+++ luizbackend/exe1.js
@@ -1,10 +1,14 @@
-let av1 = 5.0;
+let av1 = 6.0;
 let av2 = 6.0;
 let rec = 0.0;
 let rec2 = 0.0;
 let nt1;
 let nt2;
-if (rec > av1){
+
+nt1 = rec > av1 ? rec : av1;
+nt2 = rec2 > av2 ? rec2 : av2;
+let tme = (nt1 + nt2) /2
+/*if (rec > av1){
     nt1 = rec
 } else {
     nt1 = av1
@@ -15,9 +19,10 @@ if (rec2 > av2){
     nt2 = av2
 }
 let tme = nt1 + nt2;
-if(tme/2 >= 6 ){
-    console.log("Você passou! A sua Média é:"+ tme/2)
+*/
+if(tme >= 6 ){
+    console.log("Você passou! A sua Média é:"+ tme)
 } else {
-    console.log("você reprovou!" + tme/2)
+    console.log("você reprovou!" + tme)
 }
 
