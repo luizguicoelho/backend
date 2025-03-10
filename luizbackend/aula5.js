7d380450c0bd385a7d2ec50904795f95f1593860
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-03-10 12:19:10 -0300

    Add files via upload

--- luizbackend/aula5.js
+++ luizbackend/aula5.js
@@ -1,8 +1,52 @@
-let esp = ["coxinha de frango com catupiri", "pastel", "coxinha 500g", "coxinha 1kg", "risoles",]
-let vlr = ["2,5", "5", "14,5", "32,95", "3"]
+let com = ["coxinha de frango com catupiri", "pastel", "coxinha 500g", "coxinha 1kg", "risoles",]
+let vlr = ["2", "5", "15", "32", "3"]
 ct = 0;
+
+function mosCom() {
+    com.forEach((co) =>{
+        console.log(co)
+    })
+    vlr.forEach((vl) =>{
+        console.log(vl)
+    })
+
+}
+
+
+function addPre(nvPre){
+    com.push(nvPre);
+}
+function addCom(nvCom){
+    com.push(nvCom);
+}
+
+mosCom();
+
+addCom("torta");
+addCom("bolo");
+addPre("15");
+addPre("34");
+console.log("Lanches após adição");
+mosCom();
+
+
+let vlrAtua = vlr.map((vl) => (vl * 0.9));
+console.log(vlrAtua)
+
+/*
 while(ct < esp.length){
     let ct2 = ct + 1
     console.log(ct2 + " - " + esp[ct] + " R$ " + vlr[ct])
     ct = ct + 1
 }
+/*
+
+esp.push("queimada");
+console.log(esp);
+
+esp.pop();
+console.log(esp);
+
+esp.splice(0,1);
+console.log(esp);
+*/
