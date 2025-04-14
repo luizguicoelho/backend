170326fecb30a1b0a2cd1233c7a63370d0e22168
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-04-14 12:01:16 -0300

    Add files via upload

--- luizbackend/atvlivro.js
+++ luizbackend/atvlivro.js
@@ -3,10 +3,33 @@ const l = require("./livros.json");
 function mostraLivros(){
     console.log("Titulo - autor - editora - ano - sipnose - paginas - generos - preco")
     l.forEach((livro => {
-        console.log(`${livro.titulo}- Autor: ${livro.autor} - editora: ${livro.editora} - Ano: ${livro.ano} - Páginas ${livro.paginas} - Gêneros ${livro.generos} - Preço ${livro.preco} `)
+        console.log(`${livro.titulo}- Autor: ${livro.autor} - editora: ${livro.editora} - Ano: ${livro.ano} - Páginas: ${livro.paginas} - Gêneros: ${livro.generos} - Preço: ${livro.preco} `)
     }))
 }
 
+function AddLivro(titulo, autor, editora, ano, paginas, generos, preco){
+    l.push({
+        titulo: titulo,
+        autor: autor,
+        editora:  editora ,
+        ano: ano,
+        paginas: paginas,
+        generos: generos,
+        preco: preco
+    })
+    console.log("Livro Adicionado com sucesso!")
+}
+/*
+AddLivro(
+    "O Grande Tigre",
+    "Guilherme Rodrigues",
+    "Coelho e Cia",
+    2020,
+    432,
+    ["Roubo", "Falsidade", "Lavagem de dinheiro", "Aventura"],
+    37802.90
+);
+*/
 //JSON = JavaScript Object Notation
 //gera um  arquivo JSON dos livros
 function criarArquivo(){
@@ -15,6 +38,7 @@ function criarArquivo(){
     fs.writeFileSync("livros.json", lT);
 }
 
-criarArquivo();
+
+//criarArquivo();
 
 mostraLivros();
