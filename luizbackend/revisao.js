b872b81e855b251cb8d1c22db36495abd3c36cfa
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-05-14 11:28:07 -0300

    Add files via upload

--- luizbackend/revisao.js
+++ luizbackend/revisao.js
@@ -1,15 +1,23 @@
 /* Arquivo base para realização da Atividade de Revisão do 1º TRIMESTRE */
 
 // 1) Criação de variável de texto e numérica
+console.log("EXE 1")
+console.log()
+
 let nome = "luiz"
 let numérica = 21
 
 // 2) Criação de uma lista
+console.log("EXE 2")
+
 let jogos = ["Pocket Contest", "Clash Royale", "Pokemon"]
 
+console.log()
 // 3) Utilização de um laço while
+
+
 console.log("####### Exercício 3 #######")
-console.log("Jogos favoritos de Luiz são:")
+console.log(`Jogos favoritos de ${nome} são:`)
 
 c = 0
 while( c < 3){
@@ -17,7 +25,10 @@ while( c < 3){
     c++
 }
 
+console.log()
 // 4) Criação de um objeto
+console.log("EXE 4")
+
 let objeto = [{
     nome,
     nomef: "Os Vingadores",
@@ -25,7 +36,10 @@ let objeto = [{
     nota : 7.1
 }]
 
+console.log()
 // 5) Utilização de if-else
+console.log("EXE 5")
+
 if(objeto.nota < 4.0){
     console.log("O filme possui uma nota considerada baixa.")
 } else if(objeto.nota < 7.0) {
@@ -34,6 +48,7 @@ if(objeto.nota < 4.0){
     console.log("O filme é muito bom!")
 }
 
+console.log()
 // 6) Criação de uma função que analisa os dados da previsão do tempo
 const previsaoTempo = [
     {
@@ -77,12 +92,11 @@ function verificachuva(previsaoTempo){
         let dia = previsaoTempo[c]
         console.log(`Dia ${dia.dia}`)
 
-        if(dia.chanceDeChuva[c] > 50){
+        if(dia.chanceDeChuva > 50){
             console.log("levar guarda-chuva")
         } else {
             console.log("tempo agradável")
         }
-
         c++
     }
 }
